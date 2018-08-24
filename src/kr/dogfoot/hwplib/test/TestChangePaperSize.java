f04b23d8e5d271b3b08219cd3d2cab441096fabd
neolord@clipsoft.co.kr <neolord@clipsoft.co.kr>
2018-08-24 19:53:40 +0900

    용지 크기 변경하는 예제 추가...

---
+++ src/kr/dogfoot/hwplib/test/TestChangePaperSize.java
@@ -0,0 +1,71 @@
+package kr.dogfoot.hwplib.test;
+
+import kr.dogfoot.hwplib.object.HWPFile;
+import kr.dogfoot.hwplib.object.bodytext.Section;
+import kr.dogfoot.hwplib.object.bodytext.control.ControlSectionDefine;
+import kr.dogfoot.hwplib.object.bodytext.paragraph.Paragraph;
+import kr.dogfoot.hwplib.reader.HWPReader;
+import kr.dogfoot.hwplib.writer.HWPWriter;
+
+public class TestChangePaperSize {
+	private static class Size {
+		public long cx;
+		public long cy;
+		
+		public Size(long cx, long cy) {
+			this.cx = cx;
+			this.cy = cy;
+		}
+	};
+	
+	public static void main(String[] args) throws Exception {
+		String filename = "sample_hwp\\test-blank.hwp";
+		
+		HWPFile hwpFile = HWPReader.fromFile(filename);
+		if (hwpFile != null) {
+			ControlSectionDefine csd = getSectionDefineControl(hwpFile);
+			Size paperSize = getPaperSize();
+			System.out.println(paperSize.cx);
+			System.out.println(paperSize.cy);
+			
+			csd.getPageDef().setPaperWidth(paperSize.cx);
+			csd.getPageDef().setPaperHeight(paperSize.cy);
+			
+			String writePath = "sample_hwp\\test-change-paper-size.hwp";
+			HWPWriter.toFile(hwpFile, writePath);
+		}
+	}
+
+	private static ControlSectionDefine getSectionDefineControl(HWPFile hwpFile) {
+		Section s = hwpFile.getBodyText().getSectionList().get(0);
+		Paragraph firstParagraph = s.getParagraph(0);
+		
+		return (ControlSectionDefine) firstParagraph.getControlList().get(0);
+	}
+
+	private static Size getPaperSize() {
+		// B5
+    	// return new Size(51592, 72852);
+    	
+    	// B4
+        // return new Size(72852, 103180);
+    	
+    	// A4
+        // return new Size(59528, 84188);
+    	
+    	// A3
+        // return new Size(84188, 119052);
+    	
+    	// Legal
+        // return new Size(61200, 100800);
+
+    	// A5
+        // return new Size(41976, 59528);
+		
+		// other : coordinate's unit is mm ;
+		// (coordinate * 72000.0f / 254.0f + 0.5f); 
+		return new Size((long)((double) 100 * 72000.0f / 254.0f +0.5f),
+				(long)((double) 200 * 72000.f / 254.0f +0.5f));
+	}
+
+}
