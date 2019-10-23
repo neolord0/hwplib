bb7516292c1871d2dc5d8740037e79ed88da91d5
sungkyunpark <neolord@clipsoft.co.kr>
2019-10-23 16:39:04 +0900

    홀/짝 조정(페이지 번호 조정) 컨트롤 추가 -- 빠진 부분..

---
+++ src/main/java/kr/dogfoot/hwplib/reader/bodytext/paragraph/control/ForControlPageOddEvenAdjust.java
@@ -0,0 +1,13 @@
+package kr.dogfoot.hwplib.reader.bodytext.paragraph.control;
+
+import kr.dogfoot.hwplib.object.bodytext.control.ControlPageHide;
+import kr.dogfoot.hwplib.object.bodytext.control.ControlPageOddEvenAdjust;
+import kr.dogfoot.hwplib.util.compoundFile.reader.StreamReader;
+
+import java.io.IOException;
+
+public class ForControlPageOddEvenAdjust {
+    public static void read(ControlPageOddEvenAdjust pgoea, StreamReader sr) throws IOException {
+        pgoea.getHeader().getProperty().setValue(sr.readUInt4());
+    }
+}
